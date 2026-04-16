0607a69d45a981e8a5ed85f6318abc88981535d7
Asel <aselsnic@gmail.com>
2026-04-16 12:20:03 +0530

    Deployed the backend on render and updated the backend url in the source code

--- mobile-app/services/api.js
+++ mobile-app/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_BASE_URL =
-  process.env.EXPO_PUBLIC_API_URL || "http://localhost:5000/api";
+  process.env.EXPO_PUBLIC_API_URL || "https://bus-system-3lgy.onrender.com/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
